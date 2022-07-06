9133cb067794a6d4855a4274d1e73cd28eea1fa4
Joseph Ebin <josefophe@gmail.com>
2022-07-06 18:08:26 +0100

    update app

--- src/App copy.js
+++ src/App copy.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import WalletConnect from "@walletconnect/client";
 import QRCodeModal from "algorand-walletconnect-qrcode-modal";
-import algosdk from "algosdk";
+// import algosdk from "algosdk";
 import { formatJsonRpcRequest } from "@json-rpc-tools/utils";
 import dotenv from "dotenv";
 dotenv.config();
@@ -15,6 +15,13 @@ const App = () => {
   const [connected, setConnected] = useState(false);
 
   const app_address = process.env.YOUR_APPADDRESS_HERE;
+
+  const baseServer = 'https://testnet-algorand.api.purestake.io/ps2'
+    const port = '';
+    const token = {
+        'X-API-Key': process.env.API_KEY
+    }
+    const algodClient = new algosdk.Algodv2(token, baseServer, port);
 
   // Add the function for wallet connect
   
@@ -39,6 +46,7 @@ const App = () => {
     } catch (error) {
       console.log(error);
     }
+    
   }
 
 
@@ -106,8 +114,46 @@ const App = () => {
     setConnected(false);
   }
 
+  const getCount = async () => {
+    let applicationInfoResponse = await algodClient.getApplicationByID(app_address).do();
+    let globalState = []
+    globalState = applicationInfoResponse['params']['global-state']
+    console.log("count is: ", globalState[0]['value']['uint']);
+    setGlobalCount(globalState[0]['value']['uint']);
+  }
+
   const add = () => {
-    
+    // construct transaction
+    let sender = currentAccount;
+    let appArgs = [];
+    appArgs.push(new Uint8Array(Buffer.from("Add")));
+    let params = await algodClient.getTransactionParams().do();
+    const txn = algosdk.makeApplicationNoOpTxn(sender, params, app_address, appArgs);
+    let txId = txn.txID().toString();
+
+    // time to sign . . . which we have to do with walletconnect api
+    const txns = [txn]
+    const txnsToSign = txns.map(txn => {
+      const encodedTxn = Buffer.from(algosdk.encodeUnsignedTransaction(txn)).toString("base64");
+      return {
+        txn: encodedTxn,
+    };
+  });
+  const requestParams = [ txnsToSign ];
+  const request = formatJsonRpcRequest("algo_signTxn", requestParams);
+  const result = await connector.sendCustomRequest(request);
+    // have to go on phone and accept the transaction
+  const decodedResult = result.map(element => {
+    return element ? new Uint8Array(Buffer.from(element, "base64")) : null;
+  });
+    // send and await
+    await algodClient.sendRawTransaction(decodedResult).do();
+    await algosdk.waitForConfirmation(algodClient, txId, 2);
+    let transactionResponse = await algodClient.pendingTransactionInformation(txId).do();
+    console.log("Called app-id:",transactionResponse['txn']['txn']['apid']);
+    if (transactionResponse['global-state-delta'] !== undefined ) {
+      console.log("Global State updated:",transactionResponse['global-state-delta']);
+      }
   }
     const deduct = () => {
     
@@ -118,6 +164,7 @@ const App = () => {
 
   useEffect(() => {
     checkIfWalletIsConnected();
+    getCount();
         console.log('currentAccount:', currentAccount);
   }, [currentAccount])
 
